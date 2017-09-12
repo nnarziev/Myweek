f8e7233e8263dab762c4412a35fa08b84a03eb80
Nematjon Narziev <nnarziev@gmail.com>
2017-09-12 13:15:15 +0900

    Sign in/up design

--- app/src/main/java/com/example/negmat/myweek_1/SignIn.java
+++ app/src/main/java/com/example/negmat/myweek_1/SignIn.java
@@ -1,9 +1,11 @@
 package com.example.negmat.myweek_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -11,14 +13,14 @@ import butterknife.OnClick;
 
 public class SignIn extends AppCompatActivity {
 
-    @BindView(R.id.txt_uname)
-    EditText userName;
+    @BindView(R.id.txt_login)
+    EditText login;
     @BindView(R.id.txt_password)
     EditText userPassword;
     @BindView(R.id.btn_signin)
     Button btnSignIn;
     @BindView(R.id.btn_signup)
-    Button btnSignUp;
+    TextView btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +36,8 @@ public class SignIn extends AppCompatActivity {
     }
     @OnClick(R.id.btn_signup)
     public void SignUp(){
-
+        Intent i2 = new Intent(this, SignUp.class);
+        startActivity(i2);
+        overridePendingTransition( R.anim.slide_in_up, R.anim.slide_out_up );
     }
 }
--- app/src/main/java/com/example/negmat/myweek_1/SignUp.java
+++ app/src/main/java/com/example/negmat/myweek_1/SignUp.java
@@ -1,13 +1,48 @@
 package com.example.negmat.myweek_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SignUp extends AppCompatActivity {
+    boolean userRegister(){
+        return true;
+    }
+    @BindView(R.id.txt_name)
+    EditText name;
+    @BindView(R.id.txt_login)
+    EditText login;
+    @BindView(R.id.txt_password)
+    EditText password;
+    @BindView(R.id.txt_conf_password)
+    EditText confPassword;
+    @BindView(R.id.btn_register)
+    TextView btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        getSupportActionBar().setTitle("Registration");
+    }
+
+    @OnClick(R.id.btn_register)
+    public void Register(){
+        if(userRegister()){
+            Toast.makeText(this, "Registration is successfull", Toast.LENGTH_SHORT).show();
+            Intent i2 = new Intent(this, SignIn.class);
+            startActivity(i2);
+            overridePendingTransition( R.anim.slide_in_up, R.anim.slide_out_up );
+        }
     }
 }
+
